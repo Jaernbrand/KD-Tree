2a4d02fffc8b789b6c3b942af17783f00cfe3ad1
Jaernbrand <henrikjarnbrand@gmail.com>
2015-03-06 11:59:35 +0100

    insert-metoden är kanske fungerande. Behöver testas. Contains-metod behöver skrivas.

--- src/kdTree/KDTree.java
+++ src/kdTree/KDTree.java
@@ -23,6 +23,52 @@ public class KDTree<T> {
 		}
 	}
 
+	private int incrementDimension(int dimension){
+		int newDimension = ++dimension;
+		if (newDimension >= DIMENSIONS){
+			newDimension = 0;
+		}
+		return newDimension;
+	}
+	
+	private void insertInTree(Comparable[] keys, T value){
+		Node<T> currNode = root;
+		int currDimension = 0;
+		
+		Stack<Node<T>> travelStack = new Stack<Node<T>>();
+		Stack<Integer> travelDimension = new Stack<Integer>();
+		
+		boolean done = false;
+		while (!done){
+			
+			Comparable currKey = currNode.getKey(currDimension);
+			if ( currKey.compareTo(keys[currDimension]) < 0){
+				if (currNode.getLeftChild() == null){
+					currNode.setLeftChild(keys, value);
+					done = true;
+				} else {
+					//addNodeToStack(travelStack, currNode.getLeftChild());
+					travelStack.push( currNode.getLeftChild() );
+					travelDimension.push( incrementDimension(currDimension) );
+				}
+				
+			} else {
+				if (currNode.getRightChild() == null){
+					currNode.setRightChild(keys, value);
+					done = true;
+				} else {
+					//addNodeToStack(travelStack, currNode.getRightChild());
+					travelStack.push( currNode.getRightChild() );
+					travelDimension.push( incrementDimension(currDimension) );
+				}
+			}
+			
+			if ( !travelStack.isEmpty() ){
+				currNode = travelStack.pop();
+				currDimension = travelDimension.pop();
+			}
+		}
+	} // insertInTree
 	
 	public void insert(Comparable[] keys, T value){
 		if (keys == null || value == null){
@@ -32,17 +78,10 @@ public class KDTree<T> {
 			throw new IllegalArgumentException();
 		}
 		
-		Node<T> currNode = null;
 		if (root == null){
 			root = new Node<T>(keys, value);
 		} else {
-			currNode = root;
-		}
-		
-		Stack<Node<T>> travelStack = new Stack<Node<T>>();
-		while (currNode != null){
-			currNode.getLeftChild();
-			addNodeToStack();
+			insertInTree(keys, value);
 		}
 	} // insert
 	
