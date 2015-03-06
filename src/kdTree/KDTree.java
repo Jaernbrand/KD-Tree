5068523b69c8a71db8ef2a58a909844e749c7a0e
Jaernbrand <henrikjarnbrand@gmail.com>
2015-03-06 10:50:58 +0100

    Har indatakoller på insert i KDTree. Även två simpla test är skrivna.

--- src/kdTree/KDTree.java
+++ src/kdTree/KDTree.java
@@ -3,9 +3,29 @@ package kdTree;
 import java.util.Set;
 
 public class KDTree<T> {
+	
+	private int dimensions;
+	private Node<T> root;
+	
+	public KDTree(int dimensions){
+		if (dimensions < 2){
+			throw new IllegalArgumentException("USE A BINARY TREE! BITCH!");
+		}
+		
+		this.dimensions = dimensions;
+	}
 
 	public void insert(Comparable[] keys, T value){
+		if (keys == null || value == null){
+			throw new NullPointerException("Arguments can't be null.");
+		}
+		if (keys.length != dimensions){
+			throw new IllegalArgumentException();
+		}
 		
+		if (root == null){
+			root = new Node<T>(keys, value);
+		}
 	}
 	
 	public Set<T> range(Comparable[] lowest, Comparable[] highest){
--- tests/kdTree/KDTreeTester.java
+++ tests/kdTree/KDTreeTester.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class KDTreeTester {
 
+	KDTree<String> triDTree = new KDTree<String>(3);
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testToSmallK() {
+		KDTree<Integer> toSmall = new KDTree<Integer>(1);
+	}
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsertOneElement(){
+		Integer[] keys = {4, 5, 6};
+		triDTree.insert( keys, "A");
+		assertTrue( triDTree.contains(keys) );
 	}
 
 }
